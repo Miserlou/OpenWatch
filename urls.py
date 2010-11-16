e8793885a344c0873d6e07545518b7f2c48b2505
Rich Jones <rich@anomos.info>
2010-11-16 00:27:19 -0500

    Template stuff

--- urls.py
+++ urls.py
@@ -13,4 +13,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
      (r'^admin/', include(admin.site.urls)),
+     (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root':'static'}),
+     (r'^$', 'openwatch.recordings.views.root')
 )
