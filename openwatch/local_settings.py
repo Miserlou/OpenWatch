6a831a37264fbaa077a63961db934a90e6a90ddd
Rich Jones <rich@anomos.info>
2011-01-13 00:11:37 -0500

    reorg cont

--- openwatch/local_settings.py
+++ openwatch/local_settings.py
@@ -1,1 +1,8 @@
 # Your local settings go here!
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'bigvagitabluntz420@gmail.com'
+EMAIL_HOST_PASSWORD = 'incangold'
+EMAIL_PORT = 587
+
