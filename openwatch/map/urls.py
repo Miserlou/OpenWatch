d62445667b004456cda54513ecef68922a7a221c
Rich Jones <rich@anomos.info>
2012-06-25 21:56:59 -0700

    html

--- openwatch/map/urls.py
+++ openwatch/map/urls.py
@@ -8,17 +8,17 @@ info_dict = {
 urlpatterns = patterns('',
     
     
-    (r'^search/(?P<tag>[a-zA-Z0-9_. -]+)$', 'openwatch.map.views.map_tag'),
-    (r'^search/(?P<tag>[a-zA-Z0-9_. -]+)/(?P<ne_lat>[0-9.]+)/$', 'openwatch.map.views.map_tag_location'),
-    (r'^search/(?P<tag>[a-zA-Z0-9_. -]+)/(?P<ne_lat>[0-9.-]+)/(?P<ne_lon>[0-9.-]+)/(?P<sw_lat>[0-9.-]+)/(?P<sw_lon>[0-9.-]+)/$', 'openwatch.map.views.map_tag_location'),
+    #(r'^search/(?P<tag>[a-zA-Z0-9_. -]+)$', 'openwatch.map.views.map_tag'),
+    #(r'^search/(?P<tag>[a-zA-Z0-9_. -]+)/(?P<ne_lat>[0-9.]+)/$', 'openwatch.map.views.map_tag_location'),
+    #(r'^search/(?P<tag>[a-zA-Z0-9_. -]+)/(?P<ne_lat>[0-9.-]+)/(?P<ne_lon>[0-9.-]+)/(?P<sw_lat>[0-9.-]+)/(?P<sw_lon>[0-9.-]+)/$', 'openwatch.map.views.map_tag_location'),
     (r'^search/$', 'map.views.redir'),
     (r'^api/$', 'map.views.map_json'),
-    (r'^api/(?P<tag>[a-zA-Z0-9_. -]+)/$', 'map.views.map_tag_json'),
+    #(r'^api/(?P<tag>[a-zA-Z0-9_. -]+)/$', 'map.views.map_tag_json'),
     (r'^api/(?P<ne_lat>[0-9.-]+)/(?P<ne_lon>[0-9.-]+)/(?P<sw_lat>[0-9.-]+)/(?P<sw_lon>[0-9.-]+)/$', 'map.views.map_location_json'),
-    (r'^about/$', 'map.views.about'),
+    #(r'^about/$', 'map.views.about'),
     (r'^size/$','map.views.size'),
 
-    (r'^recordings/(?P<zipcode>[a-zA-Z0-9_. -]+)/', 'map.views.map_zipcode'),
+    #(r'^recordings/(?P<zipcode>[a-zA-Z0-9_. -]+)/', 'map.views.map_zipcode'),
     (r'^$','openwatch.map.views.map'),
 
 
