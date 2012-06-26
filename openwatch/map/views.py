5889ca2a707602264e4874c922c460e7ffe24601
Rich Jones <rich@anomos.info>
2012-06-25 21:44:06 -0700

    vvv

--- openwatch/map/views.py
+++ openwatch/map/views.py
@@ -22,18 +22,18 @@ from openwatch.recordings.models import Recording
 def map(request):
     #featureset = Recording.objects.filter(~Q(lat=None), ~Q(lon=None), ~Q(jtype='organic')).order_by('-date')
     #total = len(featureset)
-    total = ">50000"
+    total = "lots!"
     return render_to_response('map.html', {'total': total}, context_instance=RequestContext(request)) 
 
-def map_zipcode(request, zipcode):
-    #featureset = Recording.objects.filter(~Q(lat=None), ~Q(lon=None), ~Q(jtype='organic')).order_by('-date')
-    #total = len(featureset)
-    total = ">40000"
-    try:
-        location = Location.objects.get(zipcode=zipcode)
-    except:
-        return HttpResponseRedirect('/')
-    return render_to_response('map.html', {'total': total, 'location': location, 'zipcode': zipcode}, context_instance=RequestContext(request)) 
+# def map_zipcode(request, zipcode):
+#     #featureset = Recording.objects.filter(~Q(lat=None), ~Q(lon=None), ~Q(jtype='organic')).order_by('-date')
+#     #total = len(featureset)
+#     total = ">40000"
+#     try:
+#         location = Location.objects.get(zipcode=zipcode)
+#     except:
+#         return HttpResponseRedirect('/')
+#     return render_to_response('map.html', {'total': total, 'location': location, 'zipcode': zipcode}, context_instance=RequestContext(request)) 
 
 def size(request):
     featureset = Recording.objects.filter(~Q(lat=None), ~Q(lon=None), ~Q(jtype='organic')).order_by('-date')
@@ -43,19 +43,19 @@ def size(request):
 def redir(self):
     return HttpResponseRedirect('/')
 
-def map_tag(request, tag=None):
+# def map_tag(request, tag=None):
 
-    #0 Responses
-    try:
-        query_tag = Tag.objects.get(name=tag)
-    except Exception, e:
-        return render_to_response('map.html', {'jobs': [], 'numcareers': 0, 'tag': tag}, context_instance=RequestContext(request)) 
+#     #0 Responses
+#     try:
+#         query_tag = Tag.objects.get(name=tag)
+#     except Exception, e:
+#         return render_to_response('map.html', {'jobs': [], 'numcareers': 0, 'tag': tag}, context_instance=RequestContext(request)) 
 
-    entries = TaggedItem.objects.get_by_model(Recording, query_tag)
-    featureset = entries.filter(~Q(lat=None), ~Q(lon=None),).order_by('-date')
+#     entries = TaggedItem.objects.get_by_model(Recording, query_tag)
+#     featureset = entries.filter(~Q(lat=None), ~Q(lon=None),).order_by('-date')
 
     
-    return render_to_response('map.html', {'jobs': featureset, 'numcareers': len(featureset), 'tag': tag}, context_instance=RequestContext(request))
+#     return render_to_response('map.html', {'jobs': featureset, 'numcareers': len(featureset), 'tag': tag}, context_instance=RequestContext(request))
 
 def map_json(request):
     #featureset = Recording.objects.filter(~Q(lat=None), ~Q(lon=None), ~Q(jtype='organic')).order_by('-date')[:1000]
@@ -63,39 +63,39 @@ def map_json(request):
     resp = encode_queryset(featureset)
     return HttpResponse(resp, mimetype="application/json")
 
-def map_tag_json(request, tag):
+# def map_tag_json(request, tag):
 
-    #0 Responses
-    try:
-        query_tag = Tag.objects.get(name=tag)
-    except Exception, e:
-        return HttpResponse("{\"objects\":[]}", mimetype="application/json")
+#     #0 Responses
+#     try:
+#         query_tag = Tag.objects.get(name=tag)
+#     except Exception, e:
+#         return HttpResponse("{\"objects\":[]}", mimetype="application/json")
 
-    entries = TaggedItem.objects.get_by_model(Recording, query_tag)
-    #featureset = entries.filter(~Q(lat=None), ~Q(lon=None),).order_by('-date')
-    featureset = entries.filter().order_by('-date')
-    resp = encode_queryset(featureset)
-    return HttpResponse(resp, mimetype="application/json")
+#     entries = TaggedItem.objects.get_by_model(Recording, query_tag)
+#     #featureset = entries.filter(~Q(lat=None), ~Q(lon=None),).order_by('-date')
+#     featureset = entries.filter().order_by('-date')
+#     resp = encode_queryset(featureset)
+#     return HttpResponse(resp, mimetype="application/json")
 
 
-def map_tag_location(request, tag=None, ne_lat=0, ne_lon=0, sw_lat=0, sw_lon=0):
+# def map_tag_location(request, tag=None, ne_lat=0, ne_lon=0, sw_lat=0, sw_lon=0):
 
-    ne_lat = float(ne_lat)
-    ne_lon = float(ne_lon)
-    sw_lat = float(sw_lat)
-    sw_lon = float(sw_lon)
+#     ne_lat = float(ne_lat)
+#     ne_lon = float(ne_lon)
+#     sw_lat = float(sw_lat)
+#     sw_lon = float(sw_lon)
 
-    #0 Responses
-    try:
-        query_tag = Tag.objects.get(name=tag)
-    except Exception, e:
-        return render_to_response('map.html', {'jobs': [], 'numcareers': 0, 'tag': tag}, context_instance=RequestContext(request)) 
+#     #0 Responses
+#     try:
+#         query_tag = Tag.objects.get(name=tag)
+#     except Exception, e:
+#         return render_to_response('map.html', {'jobs': [], 'numcareers': 0, 'tag': tag}, context_instance=RequestContext(request)) 
 
-    entries = TaggedItem.objects.get_by_model(Recording, query_tag)
-    #featureset = entries.filter(lat__lt=ne_lat, lat__gt=sw_lat,lon__lt=ne_lon, lon__gt=sw_lon).order_by('-date')
-    featureset = Recording.objects.order_by('-date')
+#     entries = TaggedItem.objects.get_by_model(Recording, query_tag)
+#     #featureset = entries.filter(lat__lt=ne_lat, lat__gt=sw_lat,lon__lt=ne_lon, lon__gt=sw_lon).order_by('-date')
+#     featureset = Recording.objects.order_by('-date')
     
-    return render_to_response('map.html', {'jobs': featureset, 'numcareers': len(featureset), 'tag': tag}, context_instance=RequestContext(request))
+#     return render_to_response('map.html', {'jobs': featureset, 'numcareers': len(featureset), 'tag': tag}, context_instance=RequestContext(request))
 
 def map_location_json(request, ne_lat=0, ne_lon=0, sw_lat=0, sw_lon=0):
 
@@ -105,7 +105,7 @@ def map_location_json(request, ne_lat=0, ne_lon=0, sw_lat=0, sw_lon=0):
     sw_lon = float(sw_lon)
 
     #featureset = Recording.objects.filter(lat__lt=ne_lat, lat__gt=sw_lat,lon__lt=ne_lon, lon__gt=sw_lon).order_by('-date')
-    featureset = Recording.objects.order_by('-date').filter(~Q(location=''))[:750]
+    featureset = Recording.objects.order_by('-date').exclude(location__isnull=True).exclude(location__exact='')[:750]
 
     if len(featureset) < 1:
         return HttpResponse("{\"objects\":[]}", mimetype="application/json")
@@ -113,8 +113,8 @@ def map_location_json(request, ne_lat=0, ne_lon=0, sw_lat=0, sw_lon=0):
     resp = encode_queryset(featureset)
     return HttpResponse(resp, mimetype="application/json")
 
-def about(request):
-    return render_to_response('about.html', {}, context_instance=RequestContext(request))
+# def about(request):
+#     return render_to_response('about.html', {}, context_instance=RequestContext(request))
 
 # Encoders
 
