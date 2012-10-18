0c65c2e3926017fc88daf2d59b4918c8f4e16091
David Brodsky <davidpbrodsky@gmail.com>
2012-10-18 12:14:03 -0700

    ow theme css

--- openwatch/map/urls.py
+++ openwatch/map/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls.defaults import *
-from openwatch.recordings.models import Recording 
+from openwatch.recordings.models import Recording
 
 info_dict = {
   'queryset': Recording.objects.all(),
